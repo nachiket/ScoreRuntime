0aa8c10fbedf4c428a53e182cf177540d6476a35
Nachiket Kapre <nachiket@gmail.com>
2009-12-28 14:50:11 -0500

    Removed libperfctr... Whoa

--- testing/add8.cc
+++ testing/add8.cc
@@ -1,6 +1,6 @@
 // cctdfc autocompiled header file
 // tdfc version 1.160
-// Mon Sep 28 21:25:14 2009
+// Mon Dec 14 04:25:22 2009
 
 #include "Score.h"
 #include <errno.h>
@@ -16,79 +16,38 @@ char * add8_name="add8";
 void * add8_proc_run(void *obj) {
   return(((nonfunc_add8 *)obj)->proc_run()); }
 ScoreOperatorElement *nonfunc_add8init_instances() {
-  return(ScoreOperator::addOperator(add8_name,0,3,0));  }
+  return(ScoreOperator::addOperator(add8_name,0,2,0));  }
 ScoreOperatorElement *nonfunc_add8::instances=nonfunc_add8init_instances();
 
-nonfunc_add8::nonfunc_add8(UNSIGNED_SCORE_STREAM n_cc_a,UNSIGNED_SCORE_STREAM n_cc_b,BOOLEAN_SCORE_STREAM n_cc_c)
+nonfunc_add8::nonfunc_add8(UNSIGNED_SCORE_STREAM n_a,UNSIGNED_SCORE_STREAM n_b)
 {
   int *params=(int *)malloc(0*sizeof(int));
   addInstance(instances,params);
   char * name=mangle(add8_name,0,params);
   char * instance_fn=resolve(name);
-  if (instance_fn!=(char *)NULL) {
-    long slen;
-    long alen;
-    long blen;
-    add8_arg *data;
-    struct msgbuf *msgp;
-    data=(add8_arg *)malloc(sizeof(add8_arg));
+  assert (instance_fn==(char *)NULL);
   result=NEW_UNSIGNED_SCORE_STREAM(9);
-    data->i0=STREAM_OBJ_TO_ID(result);
-    data->i1=STREAM_OBJ_TO_ID(n_cc_a);
-    data->i2=STREAM_OBJ_TO_ID(n_cc_b);
-    data->i3=STREAM_OBJ_TO_ID(n_cc_c);
-    alen=sizeof(add8_arg);
-    slen=strlen(instance_fn);
-    blen=sizeof(long)+sizeof(long)+slen+alen;
-    msgp=(struct msgbuf *)malloc(sizeof(msgbuf)+sizeof(char)*(blen-1));
-    int sid=schedulerId();
-    memcpy(msgp->mtext,&alen,sizeof(long));
-    memcpy(msgp->mtext+sizeof(long),&slen,sizeof(long));
-    memcpy(msgp->mtext+sizeof(long)*2,instance_fn,slen);
-    memcpy(msgp->mtext+sizeof(long)*2+slen,data,alen);
-    msgp->mtype=SCORE_INSTANTIATE_MESSAGE_TYPE;
-    int res=msgsnd(sid, msgp, blen, 0) ;  
-    if (res==-1) {
-       cerr <<"nonfunc_add8 msgsnd call failed with errno=" << errno << endl;
-       exit(2);    }
-  }
-  else {
-  result=NEW_UNSIGNED_SCORE_STREAM(9);
-    declareIO(3,1);
+    declareIO(2,1);
     bindOutput(0,result,new ScoreStreamType(0,9));
-    bindInput(0,n_cc_a,new ScoreStreamType(0,8));
-    SCORE_MARKREADSTREAM(n_cc_a,globalCounter->threadCounter);
-    bindInput(1,n_cc_b,new ScoreStreamType(0,8));
-    SCORE_MARKREADSTREAM(n_cc_b,globalCounter->threadCounter);
-    bindInput(2,n_cc_c,new ScoreStreamType(0,1));
-    SCORE_MARKREADSTREAM(n_cc_c,globalCounter->threadCounter);
+    bindInput(0,n_a,new ScoreStreamType(0,8));
+    SCORE_MARKREADSTREAM(n_a,globalCounter->threadCounter);
+    bindInput(1,n_b,new ScoreStreamType(0,8));
+    SCORE_MARKREADSTREAM(n_b,globalCounter->threadCounter);
     pthread_attr_t *a_thread_attribute=(pthread_attr_t *)malloc(sizeof(pthread_attr_t));
     pthread_attr_init(a_thread_attribute);
     pthread_attr_setdetachstate(a_thread_attribute,PTHREAD_CREATE_DETACHED);
     pthread_create(&rpt,a_thread_attribute,&add8_proc_run, this);
-  }
 }
 
 void *nonfunc_add8::proc_run() {
   enum state_syms {STATE_only};
   state_syms state=STATE_only;
-  unsigned long cc_a;
-  int retime_length_0=0;
-  unsigned long *cc_a_retime=new unsigned long [retime_length_0+1];
-  for (int j=retime_length_0;j>=0;j--)
-    cc_a_retime[j]=0;
-  unsigned long cc_b;
-  int retime_length_1=0;
-  unsigned long *cc_b_retime=new unsigned long [retime_length_1+1];
-  for (int j=retime_length_1;j>=0;j--)
-    cc_b_retime[j]=0;
-  int cc_c;
-  int retime_length_2=0;
-  int *cc_c_retime=new int [retime_length_2+1];
-  for (int j=retime_length_2;j>=0;j--)
-    cc_c_retime[j]=0;
-  int *input_free=new int[3];
-  for (int i=0;i<3;i++)
+  unsigned long a;
+    a=0;
+  unsigned long b;
+    b=0;
+  int *input_free=new int[2];
+  for (int i=0;i<2;i++)
     input_free[i]=0;
   int *output_close=new int[1];
   for (int i=0;i<1;i++)
@@ -97,45 +56,9 @@ void *nonfunc_add8::proc_run() {
   while (!done) {
         {
         int eos_0=STREAM_EOS(in[0]);
+        int eofr_0=STREAM_EOFR(in[0]);
         int eos_1=STREAM_EOS(in[1]);
-        int eos_2=STREAM_EOS(in[2]);
-        if (1&&!eos_0&&!eos_1&&!eos_2) {
-          cc_a=STREAM_READ_NOACC(in[0]);
-          for (int j=retime_length_0;j>0;j--)
-            cc_a_retime[j]=cc_a_retime[j-1];
-          cc_a_retime[0]=cc_a;
-          cc_b=STREAM_READ_NOACC(in[1]);
-          for (int j=retime_length_1;j>0;j--)
-            cc_b_retime[j]=cc_b_retime[j-1];
-          cc_b_retime[0]=cc_b;
-          cc_c=STREAM_READ_NOACC(in[2]);
-          for (int j=retime_length_2;j>0;j--)
-            cc_c_retime[j]=cc_c_retime[j-1];
-          cc_c_retime[0]=cc_c;
-          if (cc_c_retime[0]) {
-            {
-              STREAM_WRITE_NOACC(out[0],(cc_a_retime[0]+cc_b_retime[0]));
-            }
-          }
-          else {
-            {
-              STREAM_WRITE_NOACC(out[0],(cc_a_retime[0]-cc_b_retime[0]));
-            }
-          }
-        }
-        else
-         done=1;
-        }  }
-  STREAM_CLOSE(out[0]);
-  STREAM_FREE(in[0]);
-  STREAM_FREE(in[1]);
-  STREAM_FREE(in[2]);
-  return((void*)NULL); }
-
-UNSIGNED_SCORE_STREAM add8(UNSIGNED_SCORE_STREAM cc_a,UNSIGNED_SCORE_STREAM cc_b,BOOLEAN_SCORE_STREAM cc_c) {
-   nonfunc_add8 *res=new nonfunc_add8(cc_a,cc_b,cc_c);
- return(res->getResult()); }
-#undef NEW_nonfunc_add8
-extern "C" void *NEW_nonfunc_add8(UNSIGNED_SCORE_STREAM i0,UNSIGNED_SCORE_STREAM i1,BOOLEAN_SCORE_STREAM i2) {
-  return((void *) (new nonfunc_add8( i0, i1, i2)));
-}
+        int eofr_1=STREAM_EOFR(in[1]);
+        if (1&&!eos_0&&!eofr_0&&!eos_1&&!eofr_1) {
+          a=STREAM_READ_NOACC(in[0]);
+          b=STREAM_READ_NOACC(in[1]);
--- testing/add8.h
+++ testing/add8.h
@@ -1,6 +1,6 @@
 // cctdfc autocompiled header file
 // tdfc version 1.160
-// Mon Sep 28 21:25:14 2009
+// Mon Dec 14 04:25:22 2009
 
 #include "Score.h"
 #include <pthread.h>
@@ -9,12 +9,11 @@ typedef struct {
    int i0;
    int i1;
    int i2;
-   int i3;
 } add8_arg;
 
 class nonfunc_add8: public ScoreOperator {
 public: 
-  nonfunc_add8(UNSIGNED_SCORE_STREAM i0,UNSIGNED_SCORE_STREAM i1,BOOLEAN_SCORE_STREAM i2);
+  nonfunc_add8(UNSIGNED_SCORE_STREAM i0,UNSIGNED_SCORE_STREAM i1);
   UNSIGNED_SCORE_STREAM getResult() { return result;}
   void *proc_run();
 private: 
@@ -22,10 +21,10 @@ private:
   UNSIGNED_SCORE_STREAM result;
   static ScoreOperatorElement *instances;
 };
-UNSIGNED_SCORE_STREAM add8(UNSIGNED_SCORE_STREAM cc_a,UNSIGNED_SCORE_STREAM cc_b,BOOLEAN_SCORE_STREAM cc_c);
+UNSIGNED_SCORE_STREAM add8(UNSIGNED_SCORE_STREAM a,UNSIGNED_SCORE_STREAM b);
 typedef nonfunc_add8* OPERATOR_nonfunc_add8;
 #define NEW_nonfunc_add8 new nonfunc_add8
 #else
 typedef void* OPERATOR_nonfunc_add8;
-void *NEW_nonfunc_add8(UNSIGNED_SCORE_STREAM i0,UNSIGNED_SCORE_STREAM i1,BOOLEAN_SCORE_STREAM i2);
+void *NEW_nonfunc_add8(UNSIGNED_SCORE_STREAM i0,UNSIGNED_SCORE_STREAM i1);
 #endif
